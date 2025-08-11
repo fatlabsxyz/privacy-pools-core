460e8ea4aed84c5756335e03d5c8148bde7189c2
casiojapi <maximosantoro@protonmail.com>
2025-08-11 15:01:46 -0300

    fix: fraxusd decimal issue (#25)
    * fix: adjust decimals in FRXUSD/USDC case
    * fix: frxusd tx fees

--- packages/relayer/src/providers/quote.provider.ts
+++ packages/relayer/src/providers/quote.provider.ts
@@ -10,10 +10,19 @@ export class QuoteProvider {
   }
 
   async quoteNativeTokenInERC20(chainId: number, addressIn: Address, amountIn: bigint): Promise<{ num: bigint, den: bigint, path: (string | number)[] }> {
-    // FIX: If FraxUSD, use USDC price instead
+    // XXX: if FRXUSD, use USDC quote but adjust for decimal difference
     if (getAddress(addressIn) === getAddress(FRAXUSD_ADDRESS)) {
-      const { in: in_, out, path } = (await uniswapProvider.quoteNativeToken(chainId, USDC_ADDRESS as Address, amountIn))!;
-      return { num: out.amount, den: in_.amount, path };
+
+      // FRXUSD has 18 decimals, USDC has 6 decimals
+      // adjust the amountIn from 18 decimals to 6 decimals
+      const DECIMAL_DIFFERENCE = 10n ** 12n;  // 18-6
+      const adjustedAmount = amountIn / DECIMAL_DIFFERENCE;
+
+      // Get the USDC quote - this returns how much ETH we need for X USDC
+      const { out, path } = (await uniswapProvider.quoteNativeToken(chainId, USDC_ADDRESS as Address, adjustedAmount))!;
+
+      // So num = ETH amount, den = FRXUSD amount in 18 decimals
+      return { num: out.amount, den: amountIn, path };
     }
 
     const { in: in_, out, path } = (await uniswapProvider.quoteNativeToken(chainId, addressIn, amountIn))!;

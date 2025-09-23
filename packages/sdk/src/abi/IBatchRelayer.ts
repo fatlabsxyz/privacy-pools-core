ba9776d1cadeb71c1512f7b2977bdcf847f90a06
casiojapi <casiopilled@gmail.com>
2025-09-23 16:39:59 -0300

    chore: format abi properly

--- packages/sdk/src/abi/IBatchRelayer.ts
+++ packages/sdk/src/abi/IBatchRelayer.ts
@@ -1,1 +1,139 @@
-export const IBatchRelayerABI = [{"type":"function","name":"MAX_RELAY_FEE_BPS","inputs":[],"outputs":[{"name":"_maxRelayFeeBPS","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"batchRelay","inputs":[{"name":"_pool","type":"address","internalType":"contract IPrivacyPool"},{"name":"_withdrawal","type":"tuple","internalType":"struct IPrivacyPool.Withdrawal","components":[{"name":"processooor","type":"address","internalType":"address"},{"name":"data","type":"bytes","internalType":"bytes"}]},{"name":"_proofs","type":"tuple[]","internalType":"struct ProofLib.WithdrawProof[]","components":[{"name":"pA","type":"uint256[2]","internalType":"uint256[2]"},{"name":"pB","type":"uint256[2][2]","internalType":"uint256[2][2]"},{"name":"pC","type":"uint256[2]","internalType":"uint256[2]"},{"name":"pubSignals","type":"uint256[8]","internalType":"uint256[8]"}]}],"outputs":[],"stateMutability":"nonpayable"},{"type":"event","name":"BatchRelayed","inputs":[{"name":"_pool","type":"address","indexed":true,"internalType":"contract IPrivacyPool"},{"name":"_recipient","type":"address","indexed":true,"internalType":"address"},{"name":"_feeRecipient","type":"address","indexed":true,"internalType":"address"},{"name":"_amountAfterFees","type":"uint256","indexed":false,"internalType":"uint256"},{"name":"_fee","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"error","name":"EmptyProofs","inputs":[]},{"type":"error","name":"InvalidBatchSize","inputs":[]},{"type":"error","name":"InvalidRelayFeeBPS","inputs":[]},{"type":"error","name":"InvalidTotalValue","inputs":[]},{"type":"error","name":"NativeAssetTransferFailed","inputs":[]},{"type":"error","name":"ZeroAddress","inputs":[]}] as const;
+export const IBatchRelayerABI = [
+  {
+    type: "function",
+    name: "MAX_RELAY_FEE_BPS",
+    inputs: [],
+    outputs: [
+      {
+        name: "_maxRelayFeeBPS",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "batchRelay",
+    inputs: [
+      {
+        name: "_pool",
+        type: "address",
+        internalType: "contract IPrivacyPool",
+      },
+      {
+        name: "_withdrawal",
+        type: "tuple",
+        internalType: "struct IPrivacyPool.Withdrawal",
+        components: [
+          {
+            name: "processooor",
+            type: "address",
+            internalType: "address",
+          },
+          {
+            name: "data",
+            type: "bytes",
+            internalType: "bytes",
+          },
+        ],
+      },
+      {
+        name: "_proofs",
+        type: "tuple[]",
+        internalType: "struct ProofLib.WithdrawProof[]",
+        components: [
+          {
+            name: "pA",
+            type: "uint256[2]",
+            internalType: "uint256[2]",
+          },
+          {
+            name: "pB",
+            type: "uint256[2][2]",
+            internalType: "uint256[2][2]",
+          },
+          {
+            name: "pC",
+            type: "uint256[2]",
+            internalType: "uint256[2]",
+          },
+          {
+            name: "pubSignals",
+            type: "uint256[8]",
+            internalType: "uint256[8]",
+          },
+        ],
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "event",
+    name: "BatchRelayed",
+    inputs: [
+      {
+        name: "_pool",
+        type: "address",
+        indexed: true,
+        internalType: "contract IPrivacyPool",
+      },
+      {
+        name: "_recipient",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "_feeRecipient",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "_amountAfterFees",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+      {
+        name: "_fee",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "error",
+    name: "EmptyProofs",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidBatchSize",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidRelayFeeBPS",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InvalidTotalValue",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "NativeAssetTransferFailed",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "ZeroAddress",
+    inputs: [],
+  },
+] as const;

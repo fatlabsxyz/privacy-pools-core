5f44396cd1915d2daf73000b0d13e0260030a08c
Daffy <daffy@fatsolutions.xyz>
2025-12-05 18:47:47 -0300

    swap interface

---
+++ packages/relayer/src/providers/swap.provider.interface.ts
@@ -0,0 +1,32 @@
+import { Address } from "@0xbow/privacy-pools-core-sdk";
+import { Hex } from "viem";
+
+type QuoteToken = { amount: bigint, decimals: number; };
+
+export type Quote = {
+  path: string[];
+  valueIn: QuoteToken;
+  valueOut: QuoteToken;
+};
+
+export interface SwapWithRefundParams {
+  chainId: number;
+  feeReceiver: Address;
+  nativeRecipient: Address;
+  tokenIn: Address;
+  feeGross: bigint;
+  refundAmount: bigint;
+  feeBase: bigint;
+}
+
+export interface QuoteInNativeTokenParams {
+  chainId: number, 
+  tokenAddress: Address, 
+  reciever?: Address,
+  amount: bigint
+}
+
+export interface SwapProvider { 
+  quoteNativeToken(params: QuoteInNativeTokenParams): Promise<Quote> 
+  swapExactInputForWeth(params: SwapWithRefundParams): Promise<Hex> 
+}
